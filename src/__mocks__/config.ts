06d193cda75f36b1ba070813e22427f315c76c7a
Elimira <khodaee.elmira@gmail.com>
2021-11-07 00:04:42 +0100

    update dependencies

--- src/__mocks__/config.ts
+++ src/__mocks__/config.ts
@@ -1,6 +1,6 @@
 import { v4 as uuid } from 'uuid';
 
-const { mongo, ...otherConfigs } = require.requireActual('../config.ts').default;
+const { mongo, ...otherConfigs } = jest.requireActual('../config.ts').default;
 
 mongo.name += `-test-${uuid()}`;
 
