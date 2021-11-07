c94f172c177b419722df79edb8d7084f15a1223c
Elimira <khodaee.elmira@gmail.com>
2021-11-07 23:50:40 +0100

    format

--- src/config.ts
+++ src/config.ts
@@ -2,30 +2,30 @@ import { IMongoModuleOptions } from './mongo/index';
 import { DocumentBuilder } from '@nestjs/swagger';
 import 'dotenv/config';
 
-const appKey        = process.env.APP_KEY        || 'some-random-string';
+const appKey = process.env.APP_KEY || 'some-random-string';
 const serverAddress = process.env.SERVER_ADDRESS || 'http://localhost:3333';
 
 const config: IConfig = {
   app: {
     port: process.env.PORT || 3333,
-    url:  serverAddress,
-    key:  appKey,
+    url: serverAddress,
+    key: appKey,
   },
   mongo: {
     name: process.env.MONGO_DB_NAME || 'Project',
     host: process.env.MONGO_HOST || 'localhost:27017',
   },
- openAPIObject: new DocumentBuilder()
-  .setTitle('Assignment')
-  .setDescription('The Web Service API descriptions')
-  .setVersion('1.0')
-  .addTag('Unity ♥')
-  .build()
+  openAPIObject: new DocumentBuilder()
+    .setTitle('Assignment')
+    .setDescription('The Web Service API descriptions')
+    .setVersion('1.0')
+    .addTag('Unity ♥')
+    .build(),
 };
 
 export default config;
 interface IConfig {
-  app:    any;
+  app: any;
   mongo: IMongoModuleOptions;
-  openAPIObject: any
-};
+  openAPIObject: any;
+}
