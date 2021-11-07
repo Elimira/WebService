87be64a5d54fa8115e3ed18190491bdb3907e17a
Elimira <khodaee.elmira@gmail.com>
2021-11-07 22:59:13 +0100

    format

--- src/mongo/mongo.decorators.ts
+++ src/mongo/mongo.decorators.ts
@@ -1,26 +1,32 @@
-
-import { Db, IndexSpecification }                  from 'mongodb';
-import { Inject, Logger }                         from '@nestjs/common';
+import { Db, IndexSpecification } from 'mongodb';
+import { Inject, Logger } from '@nestjs/common';
 import { mongoConnectionToken, mongoClientToken } from './constants';
 
 export const InjectConnection = () => Inject(mongoConnectionToken);
-export const InjectClient     = () => Inject(mongoClientToken);
-export const IndexDB = (collectionName: string, indexes: Array<IndexSpecification>): MethodDecorator => {
-	return (target, key: string | symbol, descriptor: TypedPropertyDescriptor<any>) => {
-		return {
-			value: async function (...args: any[]) {
-				const logger = new Logger();
-				const method = descriptor.value;
-				try {
-					const db: Db = this.mongoConnection;
-					await db.collection(collectionName).createIndexes(indexes);
-					logger.log(`Mongo Indexing: ${indexes.length} new indexes inserted`);
-				} catch (error) {
-					logger.error("Error: indexing failed")
-				}
-				const result = await method.apply(this, args);
-				return result;
-			}
-		} as TypedPropertyDescriptor<any>;
-	}
-}
+export const InjectClient = () => Inject(mongoClientToken);
+export const IndexDB = (
+  collectionName: string,
+  indexes: Array<IndexSpecification>,
+): MethodDecorator => {
+  return (
+    target,
+    key: string | symbol,
+    descriptor: TypedPropertyDescriptor<any>,
+  ) => {
+    return {
+      value: async function(...args: any[]) {
+        const logger = new Logger();
+        const method = descriptor.value;
+        try {
+          const db: Db = this.mongoConnection;
+          await db.collection(collectionName).createIndexes(indexes);
+          logger.log(`Mongo Indexing: ${indexes.length} new indexes inserted`);
+        } catch (error) {
+          logger.error('Error: indexing failed');
+        }
+        const result = await method.apply(this, args);
+        return result;
+      },
+    } as TypedPropertyDescriptor<any>;
+  };
+};
