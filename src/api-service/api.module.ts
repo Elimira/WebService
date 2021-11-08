0badb1981370740e3666f47d26ae4e1f416f785a
Elimira <elmira.khodaie@motorolasolutions.com>
2021-11-08 17:28:58 +0100

    Add publisher

---
+++ src/api-service/api.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
+import { ApiController } from './api.controller';
+import { ApiService } from './api.service';
+ 
+@Module({
+  imports: [ConfigModule],
+  controllers: [ApiController],
+  providers: [
+    ApiService,
+    {
+      provide: 'PUBLISH_PAYLOAD',
+      useFactory: (configService: ConfigService) => {
+        const queueName = configService.get('RABBITMQ_QUEUE_NAME');
+ 
+        return ClientProxyFactory.create({
+          transport: Transport.RMQ,
+          options: {
+            urls: ['amqp://localhost:5672'],
+            queue: queueName,
+            queueOptions: {
+              durable: true,
+            },
+          },
+        })
+      },
+      inject: [ConfigService],
+    }
+  ],
+})
+export class ApiModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,9 @@ import { AppController } from '../app/app.controller';
 import { MongoModule } from '../mongo/mongo.module';
 import { ApiController } from '../api-service/api.controller';
 import { ApiService } from '../api-service/api.service';
+import { ApiModule } from 'src/api-service/api.module';
 @Module({
-  imports: [MongoModule.forRoot(config.mongo)],
+  imports: [ApiModule, MongoModule.forRoot(config.mongo)],
   controllers: [AppController, ApiController],
   providers: [ApiService],
 })
