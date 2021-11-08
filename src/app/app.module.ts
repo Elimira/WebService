13977ccc0a5ac1fd8e086485a9bfc1cb9588917e
Elimira <khodaee.elmira@gmail.com>
2021-11-08 12:04:45 +0100

    initiate a microservice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,25 @@ import { AppController }  from '../app/app.controller';
 import { MongoModule }    from '../mongo/mongo.module';
 import { ApiController }  from '../api-service/api.controller';
 import { ApiService }     from '../api-service/api.service';
+import { Transport, ClientsModule } from '@nestjs/microservices';
 
 @Module({
   imports: [    
      MongoModule.forRoot(config.mongo),
+     ClientsModule.register([
+      {
+        name: 'MATH_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'cats_queue',
+          noAck: false,
+          queueOptions: {
+            durable: false
+          },
+        },
+      },
+    ]),
   ],
   controllers: [AppController, ApiController],
   providers  : [ApiService],
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,22 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import config from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const microserviceTcp = app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'], //TODO: read from config file  TODO: Dockerfile rabbitmq-server 2- rabbitmq-plugins enable rabbitmq_management 3- http://localhost:15672/
+      queue: 'cats_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -16,6 +28,7 @@ async function bootstrap() {
   app.enableCors();
   const document = SwaggerModule.createDocument(app, config.openAPIObject);
   SwaggerModule.setup('api', app, document);
+  await app.startAllMicroservices();
   await app.listen(config.app.port);
 }
 bootstrap();
