dbcc731eb5d6dea4a15dff4a9cb4d68bb0979b52
Elimira <khodaee.elmira@gmail.com>
2021-11-08 01:58:34 +0100

    babye extra codes

--- test/api.e2e-spec.ts
+++ test/api.e2e-spec.ts
@@ -1,41 +1,10 @@
 import * as request from 'supertest';
 import { app, db } from './setup';
-import { dbDocuments } from '../src/sample-data/index';
-import { ObjectID } from 'mongodb';
 
 describe('APIController (e2e)', () => {
-  beforeAll(async () => {
-    await db.collection('data').insertMany(dbDocuments);
-  });
-
   afterAll(async () => {
     await db.collection('data').drop();
   });
-
-  // it('/ (getSubTrees): It should return all ancestors of a given node', async () => {
-  //   const id = new ObjectID('5da9aef00b5e8d41605d12fd');
-  //   const parentInfo = await db.collection('tree').findOne({ _id: id });
-  //   const results = await db.collection('tree')
-  //     .find({ ancestors: parentInfo.name })
-  //     .sort({ height: 1 })
-  //     .toArray();
-  //   const mongoStreamingCompatibleResult = results.map(res => (
-  //     {
-  //       ...res,
-  //       _id: (res._id).toString(),
-  //       parentId: (res.parentId).toString()
-  //     }))
-  //   await request(app.getHttpServer())
-  //     .get('/tree/search5da9aef00b5e8d41605d12fd')
-  //     .expect(200)
-  //     .expect({ status: 200, res: mongoStreamingCompatibleResult })
-  // });
-
-  // it('/ (getSubTrees): Bad Request with invalid request param', async () => {
-  //   await request(app.getHttpServer())
-  //     .get('/tree/search5da9aef00b5e8d41605d12fg')
-  //     .expect({ status: 404, res: [] })
-  // });
 
   it('/ (takeWebData): It should persist a web data into the database', async () => {
     const response = await request(app.getHttpServer())
@@ -48,7 +17,7 @@ describe('APIController (e2e)', () => {
           baz: 'bang',
         },
         send_from_ip: '19.117.63.126',
-        priority: 4
+        priority: 4,
       })
       .expect(201)
       .set('created', 'application/json');
@@ -65,7 +34,7 @@ describe('APIController (e2e)', () => {
           foo: 'foo without baz',
         },
         send_from_ip: '19.117.63.126',
-        priority: 4
+        priority: 4,
       })
       .expect(201)
       .set('created', 'application/json');
